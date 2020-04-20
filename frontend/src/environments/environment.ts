0a8169a53360f68ff365b6858f0634be7c313485
Hinrich Riedel <hinrich.riedel@joblift.de>
2020-04-20 18:09:47 +0200

    chore: configure for kubernetes on aws

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://aeef837c59bb84cefbdc672e59eba3bc-854597367.eu-central-1.elb.amazonaws.com:8080/api/v0"
 };
 
 /*
